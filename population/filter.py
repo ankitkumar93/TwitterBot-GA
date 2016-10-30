da94b4f3251fe672436247b1eb3425fb15955186
apurohi <apurohi@ncsu.edu>
2016-10-30 08:00:31 -0400

    filter.py: Created the filter module to allow filtration of keyword lists based on the computed reach for each keyword

---
+++ population/filter.py
@@ -0,0 +1,39 @@
+import json
+
+from apis.tweet import TweetHelper
+
+'''
+Author: Anand Purohit
+Filter module to ensure that we only consider those tweets
+that possess a score that is greater than a threshold score
+'''
+class Filter:
+    '''
+    Filter class
+    '''
+
+    def __init__(self, args):
+        self.logger = args['logger']
+        self.tweetHelper = args['tweetHelper']
+        self.filterThreshold = args['filterThreshold']
+
+    def calc_score_for_keyword(self, keyword):
+        users = self.tweetHelper.get_users_who_mentioned_keyword(keyword)
+        reach = 0
+        for user in users:
+            reach += self.tweetHelper.get_followers_count(user)
+        return reach
+
+    def filter_from_list(self, keywordList):
+        filteredKeywords = list()
+        for keyword in keywordList:
+            score = self.calc_score_for_keyword(keyword)
+            if score < self.filterThreshold:
+                filteredKeywords.append(keyword)
+
+        return filteredKeywords
+
+config = json.load(open('config.json'))
+helper = TweetHelper({"logger": None, "keyPath": config['key_path']})
+filter = Filter({"logger": None, "tweetHelper": helper, "filterThreshold": config['filter_threshold']})
+filter.calc_score_for_keyword("Titanfall 2")

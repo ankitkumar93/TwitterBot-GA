3b534bc7405e16095d0d5b7ffeeceafb063d1199
akumar18 <akumar18@ncsu.edu>
2016-12-03 03:03:07 -0500

    Done

--- src/apis/streamer.py
+++ src/apis/streamer.py
@@ -18,31 +18,19 @@ class StreamListener(tweepy.StreamListener):
     '''
 
     def setup(self, args):
-        self.config = json.load(open(args['stream_path']))
-        self.tagger = Tagger(dict(tags_path=args['tags_path'], logger=args['logger']))
+        self.tagger = Tagger(dict(logger=args['logger']))
         self.dbhelper = DBHelper(dict(logger=args['logger']))
 
 
     def on_status(self, status):
         # Process tweet and add to DB
-        # Filter on Author Count
-        author = status.author
-        if author is not None:
-            followers_count = author.followers_count
-            if followers_count < self.config['followers_count']:
-                return
-        else:
-            return
-
-        # Filter on Tags
+        tweet_id = status.id
+        followers_count = status.author.followers_count
+        author_id = status.author.id
         tags = self.tagger.tagtweet(status.text)
-        if tags is not None:
-            # Add Tweet to DB
-            tweet_id = status.id
-            author_id = status.author.id
-            tweet = dict(tweetid=tweet_id, authorid=author_id, tags=tags)
-            print(tweet)
-            #self.dbhelper.add_tweet(tweet)
+        
+        tweet = dict(tweetid=tweet_id, authorid=author_id, tags=tags, followers=followers_count)
+        self.dbhelper.add_tweet(tweet)
 
 
     def on_error(self, status_code):
--- src/apis/tagger.py
+++ src/apis/tagger.py
@@ -1,7 +1,4 @@
 import nltk
-import json
-import os
-from collections import Counter
 
 '''
 Author: Ankit Kumar
@@ -12,27 +9,10 @@ Tags a tweet using parts of speech tags
 class Tagger:
     def __init__(self, args):
         self.logger = args['logger']
-        self.config = json.load(open(args['tags_path']))
 
     def tagtweet(self, text):
         tokens = nltk.word_tokenize(text)
         tagged_text = nltk.pos_tag(tokens)
-
-        return self.filtertweet(tagged_text)
-
-    def filtertweet(self, tagged_text):
         words, tags = zip(*tagged_text)
-        tag_counts = Counter(tags)
-        nn_tag = 'NN'
-        jj_tag = 'JJ'
-        tag_len = len(tags)
-
-        if nn_tag not in tag_counts or tag_counts[nn_tag] != self.config['nn_count']:
-            return None
-        elif jj_tag not in tag_counts or tag_counts[jj_tag] != self.config['jj_count']:
-            return None
-
-        if tag_len < self.config['min_tags'] or tag_len > self.config['max_tags']:
-            return None
 
         return tags

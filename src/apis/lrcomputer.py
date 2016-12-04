5f42d4cc4b607fadc4f6a303cd227b71779f13f9
akumar18 <akumar18@ncsu.edu>
2016-12-04 08:47:47 -0500

    Fixed LRComputer config

--- src/apis/lrcomputer.py
+++ src/apis/lrcomputer.py
@@ -9,7 +9,7 @@ LR Score Computer: Computes the LR Score for Tweets
 class LRComputer:
     def __init__(self, args):
         self.logger = args['logger']
-        self.tweethelper = TweetHelper(dict(logger=args['logger'], keyPath=args['keyPath']))
+        self.tweethelper = TweetHelper(dict(logger=args['logger'], keyPath=args['key_path']))
 
     def compute(tweetid):
         # Compute LR Score

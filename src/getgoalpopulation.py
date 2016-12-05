5b6ac5351e2520c2e8095983375088348719faae
akumar18 <akumar18@ncsu.edu>
2016-12-05 15:43:14 -0500

    Added scripts for automating running of ga

---
+++ src/getgoalpopulation.py
@@ -0,0 +1,29 @@
+import sys
+import math
+from pymongo import MongoClient
+
+'''
+Author: Ankit Kumar
+Get Goal Population: Returns different goal population sizes to run GA on
+'''
+
+# Globals
+client = MongoClient('mongodb://localhost:27017')
+db = client['tweet_ga']
+collection = db['filtered_tweets']
+
+# Functions
+def main():
+    lr_threshold = sys.argv[1]
+    goalMaxSize = collection.count(
+                    {"lrscore": {
+                        "$gt": lr_threshold
+                        }
+                    })
+
+    goalSizes = [1, math.log(goalMaxSize), math.sqrt(goalMaxSize), goalMaxSize/2]
+    print(" ".join(goalSizes))
+
+
+if "__name__" == "__main__":
+    main()
---
+++ src/run_ga.sh
@@ -0,0 +1,26 @@
+# Runs the Genetic Algorithm
+# Works for different parameters
+
+# Commands
+gacmd="./twitter_ga ga"
+pycmd="python getgoalpopulation.py"
+
+# Globals
+lr_threshold="0.34"
+logdir=$1
+logfile_prefix="ga_log_"
+logfile_ext=".log"
+outfile=$2
+
+# Fetch Goal Population Size
+goalpop=$($pycmd $lr_threshold)
+
+
+# Run GA for different Goal Population Sizes
+for size in $goalpop
+do
+    runningtime=$(time $gacmd -l $logdir/$logfile_prefix$size$logfile_ext -g $size)
+    outlog="Genetic Algorithm: Size: $size RunningTime: $runningtime"
+    echo $outlog
+    echo $outlog >> $outfile
+done

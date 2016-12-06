7a808df1a8ed4d70513e1e37784df35ce8944ccf
akumar18 <akumar18@ncsu.edu>
2016-12-05 19:01:17 -0500

    fixed len in ga_operators.py

--- src/evolution/ga_operators.py
+++ src/evolution/ga_operators.py
@@ -48,7 +48,7 @@ class GAOperators:
     '''
     def crossover(self, child1, child2):
         random.seed(64)
-        indexRange = min(max(child1['tags']), max(child2['tags']))
+        indexRange = min(len(child1['tags']), len(child2['tags']))
         crossoverPoint = random.randint(0, indexRange)
         if random.random() < self.crossoverProbability:
             part11 = child1['tags'][:crossoverPoint]
